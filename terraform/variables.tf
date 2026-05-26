61f8998c3bc76d38c09e21e5a58ce1f10c27921c
Augusto Yunes <augusto.yunes@accenture.com>
2026-05-26 18:55:17 -0300

    applying tf to aws projects account

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Project name for resource naming"
   type        = "string"
-  default     = "topics-backend"
+  default     = "ppaty"
 }
 
 variable "environment" {
